fa25fe451d7aa8385f888c89ab7953e7fb8773d7
Rahul <zentrades@ZenTradess-MacBook-Air.local>
2025-05-25 23:09:57 +0530

    Adding model, lighting, and particles

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,13 +1,18 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import { OrbitControls } from '@react-three/drei'
+import { useMediaQuery } from 'react-responsive';
+import Room from './Room';
+import HeroLights from './HeroLights';
+import Particles from './Particles';
 
 const HeroExperience = () => {
-    const isTablet = false;
+    const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
+    const isMobile = useMediaQuery({query: '(max-width: px)'});
   return (
     <Canvas camera={{position: [0,0,15], fov: 45}}>
-        <ambientLight intensity={0.2} color="#1a1a40"/>
-        <directionalLight position={[5,5,5]} intensity={1} />
+        <HeroLights/>
+        <Particles count={100}/>
         <OrbitControls 
         enablePan={false}
         enableZoom={!isTablet}
@@ -16,10 +21,13 @@ const HeroExperience = () => {
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
         />
-        <mesh>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial color="blue"/>
-        </mesh>
+        <group
+        scale={isMobile?0.7:1}
+        position={[0,-3.5,0]}
+        rotation={[0, -Math.PI/4, 0]}
+        >
+            <Room />
+        </group>
     </Canvas>
   )
 }
